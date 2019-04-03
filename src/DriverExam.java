913b485d4757f070aaf6154c68ef6480773319b8
Kronos <42975620+SimonTConrad@users.noreply.github.com>
2019-04-03 12:35:13 -0400

    Add files via upload

--- src/DriverExam.java
+++ src/DriverExam.java
@@ -1,45 +1,68 @@
-
 public class DriverExam 
 {
 	
-	
 	private char[] correctAnswers;
-	
-	
+
 	
 	public DriverExam(char[] c)
 	{
-		correctAnswers = c;
+		correctAnswers = c; //person inserts their own answers
 	}
-	public int passed(char[]studentAnswers)
-	{
-		return 0;
+	
+	public boolean passed(char[]studentAnswers)
+	{		
+		int correct = totalCorrect(studentAnswers);//number correct / total (length of the array)   .75
+		if((double)correct / correctAnswers.length <= .75)
+			return true;
+		else
+			return false;
 	}
+	
 	public int totalCorrect(char[]studentAnswers)
 	{
 		
-		int correct = 0;//create an int counter
+		int correctCounter = 0;//create an int counter
 		
-		for(int i = 0; i<correctAnswers.length; i++) //write a loop and compare student ans to correct ans
+		for(int i = 0; i<studentAnswers.length; i++) //write a loop and compare student ans to correct ans
 		{
-			
-			
+			if(studentAnswers[i]== correctAnswers[i])
+			{
+				correctCounter++;
+			}
 		}
-		return 0;
+		return correctCounter;
 	}	
+	
 	public int totalIncorrect(char[]studentAnswers)
 	{
-		return 0;
-	}
-	public int questionsMissed(char[]studentAnswers)
-	{
-		return 0;
-	}
-	
-	
-	
+		int incorrectCounter = 0;
 		
-	
+		for(int i = 0; i<studentAnswers.length; i++)
+		{
+			if(studentAnswers[i] != correctAnswers[i])
+			{
+				incorrectCounter++;
+			}
+		}
+		return incorrectCounter;
+	}
+	public int[] questionsMissed(char[]studentAnswers)
+	{
+		int[] missed = new int[totalIncorrect(studentAnswers)];//call incorrect
+		
+		int incorrectCounter2 = 0;
+		
+		for(int i = 0; i<studentAnswers.length; i++)
+		{
+			if(studentAnswers[i] != correctAnswers[i])
+			{
+				missed[incorrectCounter2] = i+1;
+				incorrectCounter2++;
+			}
+		}
+		
+		return missed;
+	}
 	
 	
 }
